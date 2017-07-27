7bdbf1cc990ec454e2b9028763225ef79945fe16
Ewa Jackiewicz <jackiewicz.ewa@wp.pl>
2017-07-27 22:05:51 +0100

    initial commit

---
+++ js/ClientApp.jsx
@@ -0,0 +1,22 @@
+const MyTitle = function(props) {
+  return (React.createElement('div', null, React.createElement('h1', {
+    style: {
+      color: props.color
+    }
+  }, props.title)))
+}
+
+const MyFirstComponent = function() {
+  return React.createElement('div', null, React.createElement(MyTitle, {
+    title: 'Game of Thrones',
+    color: 'YellowGreen'
+  }), React.createElement(MyTitle, {
+    title: 'Stranger Things',
+    color: 'GreenYellow'
+  }), React.createElement(MyTitle, {
+    title: 'The OA',
+    color: 'LimeGreen'
+  }));
+};
+
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById("app"));
